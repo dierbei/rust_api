ff2fd6934d9cc3b11067e6b2ed612cdb10780c9c
和兑 <chenzhangchao@cai-inc.com>
2023-04-09 20:02:23 +0800

    fix: 修复查询美女视频url

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+extern crate core;
+
 pub mod route;
 pub mod telemetry;
 
--- src/route/miss_short_video.rs
+++ src/route/miss_short_video.rs
@@ -2,6 +2,8 @@ use crate::ApiResult;
 use actix_web::get;
 use actix_web::web::Json;
 use actix_web::Responder;
+use percent_encoding::{utf8_percent_encode, AsciiSet, CONTROLS};
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -18,6 +20,19 @@ struct MissShortVideoResponse {
 
 #[get("/miss_short_video")]
 async fn miss_short_video() -> impl Responder {
+    // 定义需要编码的字符集，这里包含了所有控制字符和空格字符
+    const FRAGMENT: &AsciiSet = &CONTROLS
+        .add(b' ')
+        .add(b'"')
+        .add(b'#')
+        .add(b'<')
+        .add(b'>')
+        .add(b'?')
+        .add(b'`')
+        .add(b'{')
+        .add(b'|')
+        .add(b'}');
+
     let resp = reqwest::get("https://v.api.aa1.cn/api/api-dy-girl/index.php?aa1=json")
         .await
         .unwrap()
@@ -25,7 +40,17 @@ async fn miss_short_video() -> impl Responder {
         .await
         .unwrap();
 
+    let re = Regex::new(r"/([^/]+)\.mp4").unwrap();
+    let cap = re.captures(&resp.mp4).unwrap();
+    let filename = cap.get(1).unwrap().as_str();
+    let output_str = utf8_percent_encode(filename, FRAGMENT).to_string();
+
+    // https://v.api.aa1.cn/api/api-dy-girl/video/0084.mp434%E5%8C%85%20api.aa1.cn%20%20%E5%85%8D%E8%B4%B9%E8%A7%86%E9%A2%91API.mp4
     Json(ApiResult::success(Some(MissShortVideoResponse {
-        mp4: resp.mp4,
+        mp4: format!(
+            "https://v.api.aa1.cn/api/api-dy-girl/video/{}.mp4",
+            output_str
+        ),
+        // mp4: resp.mp4,
     })))
 }
